bd5781e5836396c22e4224d23aafef7148be0ee0
Krzysztof Nowak <kiryx7@gmail.com>
2015-08-20 00:32:15 +0200

    update

--- bin/current_directory.py
+++ bin/current_directory.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python
 import os
+import getpass
+username = getpass.getuser()
 
 path = os.getcwd()
-path= path.replace('/home/alice', '~')
+path = path.replace('/home/{}'.format(username), '~')
 dirs = path.split('/')[:]
 result = ''
 for i in xrange(0, len(dirs)):
-  if i:
-    result += '/'
-  if i < len(dirs) - 3 and dirs[i] != '~':
-    result += '..'
-  else:
-    result += dirs[i]
-print "%s" % result ,
+    if i:
+        result += '/'
+    if i < len(dirs) - 3 and dirs[i] != '~':
+        result += '..'
+    else:
+        result += dirs[i]
+print "%s" % result,
