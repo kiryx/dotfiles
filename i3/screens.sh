ea69da4aa15c422b2852f680d940ee75466ee8a3
Krzysztof Nowak <k.nowak@cern.ch>
2016-12-02 20:18:11 +0100

    screens fix

--- i3/screens.sh
+++ i3/screens.sh
@@ -8,10 +8,10 @@ SECOND="DP1"
 xrandr --output $SECOND --off --output $FIRST --auto
 
 i3-nagbar -m "SCREEN CONFIG UTILITY (run 'arandr' for config)" -t warning \
-	-b "DP1" "xrandr --output $SECOND --auto --dpi 96 --output $FIRST --off; swapkeys; setxkbmap pl -option caps:swapescape" \
-	-b "Scale XPS" "xrandr --output $FIRST --scale 0.5x0.5" \
-    -b "XPS @ Home"  "xrandr --output eDP1 --auto --output DP1 --auto --left-of eDP1" \
-	-b "$FIRST" "xrandr --output $FIRST --dpi 192 --scale 1x1"
+    -b "DP1" "xrandr --output $SECOND --auto --dpi 96 --output $FIRST --off; swapkeys; setxkbmap pl -option caps:swapescape" \
+    -b "Scale XPS" "xrandr --output $FIRST --scale 0.5x0.5" \
+    -b "XPS @ Home"  "xrandr --output eDP1 --dpi 160 --scale 1x1 --auto --output DP1 --dpi 160 --scale 1x1 --auto --right-of eDP1" \
+    -b "$FIRST" "xrandr --output $FIRST --dpi 160 --scale 1x1"
 
 # Original Script by ebrnd ( http://ebrnd.de/?p=501 )
 ##!/bin/sh
@@ -19,8 +19,8 @@ i3-nagbar -m "SCREEN CONFIG UTILITY (run 'arandr' for config)" -t warning \
 #xrandr --output VGA-0 --off --output DVI-0 --off --output LVDS --auto
 #
 #i3-nagbar -m "EBRND'S SUPER-COOL I3WM SCREEN CONFIG UTILITY" -t warning \
-#	-b "LVDS + DVI"  "xrandr --output VGA-0 --off --output LVDS --auto --output DVI-0 --auto --right-of LVDS" \
-#	-b "LVDS + VGA"  "xrandr --output DVI-0 --off --output LVDS --auto --output VGA-0 --auto --right-of LVDS" \
-#	-b "CAPS OFF"    "python /home/$USER/.local/bin/caps_lock_off.py"
+#   -b "LVDS + DVI"  "xrandr --output VGA-0 --off --output LVDS --auto --output DVI-0 --auto --right-of LVDS" \
+#   -b "LVDS + VGA"  "xrandr --output DVI-0 --off --output LVDS --auto --output VGA-0 --auto --right-of LVDS" \
+#   -b "CAPS OFF"    "python /home/$USER/.local/bin/caps_lock_off.py"
 #
 #sh ~/.fehbg 
